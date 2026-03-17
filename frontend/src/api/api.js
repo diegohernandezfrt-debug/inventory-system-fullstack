44f076dd0b1a9314e0598c5194d4090931f25413
Diego Francisco Hernández Ramírez <diegohernandezfrt@gmail.com>
2026-03-17 17:14:18 -0600

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://inventory-system-fullstack-production.up.railway.app"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 export default api;
