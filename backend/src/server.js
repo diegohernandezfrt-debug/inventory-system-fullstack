5998e5d815d99cce679bbae6666b5d2daa7befba
Diego Francisco Hernández Ramírez <diegohernandezfrt@gmail.com>
2026-03-16 20:56:50 -0600

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -29,7 +29,8 @@ app.get('/', async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
